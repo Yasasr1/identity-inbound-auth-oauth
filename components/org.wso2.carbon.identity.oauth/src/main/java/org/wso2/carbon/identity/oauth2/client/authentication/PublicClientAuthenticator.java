7b05d419f7e946aaba0ca36c5d68d3ab3019de8b
ZiyamSanthosh <santhoshziyam@gmail.com>
2023-12-08 17:43:28 +0530

    Remove redundant warn log

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/client/authentication/PublicClientAuthenticator.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/client/authentication/PublicClientAuthenticator.java
@@ -92,8 +92,6 @@ public class PublicClientAuthenticator extends AbstractOAuthClientAuthenticator 
         if (!publicClientSupportedGrantTypes.isEmpty() && grantTypes != null) {
             for (Object grantType : grantTypes) {
                 if (!publicClientSupportedGrantTypes.contains(grantType.toString())) {
-                    log.warn("The request contained grant type : '" + grantType + "' which is not " +
-                            "allowed for public clients.");
                     return false;
                 }
             }
