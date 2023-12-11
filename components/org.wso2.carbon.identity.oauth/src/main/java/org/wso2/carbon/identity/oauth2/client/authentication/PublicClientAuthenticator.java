0c5c8c4c0b3d0d1ccf1d45784d7800a1411aeba5
ZiyamSanthosh <santhoshziyam@gmail.com>
2023-12-11 09:51:35 +0530

    Add debug log

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/client/authentication/PublicClientAuthenticator.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/client/authentication/PublicClientAuthenticator.java
@@ -92,6 +92,10 @@ public class PublicClientAuthenticator extends AbstractOAuthClientAuthenticator 
         if (!publicClientSupportedGrantTypes.isEmpty() && grantTypes != null) {
             for (Object grantType : grantTypes) {
                 if (!publicClientSupportedGrantTypes.contains(grantType.toString())) {
+                    if (log.isDebugEnabled()) {
+                        log.debug("The request contained grant type : '" + grantType + "' which is not " +
+                                "allowed for public clients.");
+                    }
                     return false;
                 }
             }
