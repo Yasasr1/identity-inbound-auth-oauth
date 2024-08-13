ee96661086f1566c364ca1585c72e8e41af3ecaf
bhagyasakalanka <bhagyashirapakalanka.16@cse.mrt.ac.lk>
2024-08-13 16:38:59 +0530

    Fix userinfo retriaval with post call

--- components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/user/impl/UserInforRequestDefaultValidator.java
+++ components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/user/impl/UserInforRequestDefaultValidator.java
@@ -22,14 +22,17 @@ import org.apache.oltu.oauth2.common.error.OAuthError;
 import org.wso2.carbon.identity.oauth.user.UserInfoEndpointException;
 import org.wso2.carbon.identity.oauth.user.UserInfoRequestValidator;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.HttpMethod;
 import javax.ws.rs.core.HttpHeaders;
 
 /**
@@ -40,28 +43,75 @@ import javax.ws.rs.core.HttpHeaders;
 public class UserInforRequestDefaultValidator implements UserInfoRequestValidator {
 
     private static final String US_ASCII = "US-ASCII";
-    private static final String ACCESS_TOKEN_PARAM = "access_token";
+    private static final String ACCESS_TOKEN_PARAM = "access_token=";
     private static final String BEARER = "Bearer";
+    private static final String CONTENT_TYPE_HEADER_VALUE = "application/x-www-form-urlencoded";
+
 
     @Override
     public String validateRequest(HttpServletRequest request) throws UserInfoEndpointException {
 
         String authzHeaders = request.getHeader(HttpHeaders.AUTHORIZATION);
-        String accessToken = request.getParameter(ACCESS_TOKEN_PARAM);
-        if (StringUtils.isBlank(authzHeaders) && StringUtils.isNotBlank(accessToken)) {
-            return accessToken;
-        }
+        if (authzHeaders == null) {
+            String contentTypeHeaders = request.getHeader(HttpHeaders.CONTENT_TYPE);
+            // To validate the Content_Type header.
+            if (StringUtils.isEmpty(contentTypeHeaders)) {
+                throw new UserInfoEndpointException(OAuthError.ResourceResponse.INVALID_REQUEST,
+                        "Authorization or Content-Type header is missing");
+            }
 
-        if (StringUtils.isBlank(authzHeaders)) {
-            throw new UserInfoEndpointException(OAuthError.ResourceResponse.INVALID_REQUEST, "Bearer token missing");
+            // Restricting passing the access token via request body in GET requests.
+            if (HttpMethod.GET.equals(request.getMethod())) {
+                throw new UserInfoEndpointException(OAuthError.ResourceResponse.INVALID_REQUEST,
+                        "Authorization header is missing");
+            }
+            if ((CONTENT_TYPE_HEADER_VALUE).equals(contentTypeHeaders.trim())) {
+                StringBuilder stringBuilder = readRequestBody(request);
+                String[] arrAccessToken = new String[2];
+                String requestBody = stringBuilder.toString();
+                String[] arrAccessTokenNew;
+                // To check whether the entity-body consist entirely of ASCII [USASCII] characters.
+                if (!isPureAscii(requestBody)) {
+                    throw new UserInfoEndpointException(OAuthError.ResourceResponse.INVALID_REQUEST,
+                            "Body contains non ASCII characters");
+                }
+                if (requestBody.contains(ACCESS_TOKEN_PARAM)) {
+                    arrAccessToken = requestBody.trim().split(ACCESS_TOKEN_PARAM);
+                    if (arrAccessToken[1].contains("&")) {
+                        arrAccessTokenNew = arrAccessToken[1].split("&", 2);
+                        return arrAccessTokenNew[0];
+                    }
+                }
+                return arrAccessToken[1];
+            } else {
+                throw new UserInfoEndpointException(OAuthError.ResourceResponse.INVALID_REQUEST,
+                        "Content-Type header is wrong");
+            }
         }
-
         String[] authzHeaderInfo = authzHeaders.trim().split(" ");
         if (authzHeaderInfo.length < 2 || !BEARER.equals(authzHeaderInfo[0])) {
-            throw new UserInfoEndpointException(OAuthError.ResourceResponse.INVALID_REQUEST, "Bearer token missing");
-        }
 
+            throw new UserInfoEndpointException(OAuthError.ResourceResponse.INVALID_REQUEST,
+                    "Bearer token missing");
+        }
         return authzHeaderInfo[1];
+
+    }
+
+    private static StringBuilder readRequestBody(HttpServletRequest request) throws UserInfoEndpointException {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        Scanner scanner;
+        try {
+            scanner = new Scanner(request.getInputStream(), StandardCharsets.UTF_8.name());
+        } catch (IOException e) {
+            throw new UserInfoEndpointException(OAuthError.ResourceResponse.INVALID_REQUEST,
+                    "can not read the request body");
+        }
+        while (scanner.hasNextLine()) {
+            stringBuilder.append(scanner.nextLine());
+        }
+        return stringBuilder;
     }
 
     public static boolean isPureAscii(String requestBody) {
