34eb5cf92d34000f35213c7f6c3ce50ef7684d6c
sadilchamishka <sandilchamishka@gmail.com>
2026-03-06 19:08:08 +0530

    Remove regex validation for scope names

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/OAuth2ScopeService.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/OAuth2ScopeService.java
@@ -49,7 +49,6 @@ import static org.wso2.carbon.identity.oauth2.Oauth2ScopeConstants.ErrorMessages
  */
 public class OAuth2ScopeService {
     private static final Log log = LogFactory.getLog(OAuth2ScopeService.class);
-    private static final String SCOPE_VALIDATION_REGEX = "^[^?#()]*$";
 
     /**
      * Register a scope with the bindings
@@ -698,7 +697,6 @@ public class OAuth2ScopeService {
     private void addScopePreValidation(Scope scope) throws IdentityOAuth2ScopeClientException {
 
         validateScopeName(scope.getName());
-        validateRegex(scope.getName());
         validateDisplayName(scope.getDisplayName());
     }
 
@@ -728,15 +726,6 @@ public class OAuth2ScopeService {
                     ERROR_CODE_BAD_REQUEST_SCOPE_NAME_NOT_SPECIFIED, null);
         }
         validateWhiteSpaces(scopeName);
-    }
-
-    private void validateRegex(String scopeName) throws IdentityOAuth2ScopeClientException {
-
-        Pattern regexPattern = Pattern.compile(SCOPE_VALIDATION_REGEX);
-        if (!regexPattern.matcher(scopeName).matches()) {
-            throw Oauth2ScopeUtils.generateClientException
-                    (ERROR_CODE_BAD_REQUEST_SCOPE_NAME_NOT_SATIFIED_THE_REGEX, scopeName);
-        }
     }
 
     /**
